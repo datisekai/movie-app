3dbc9ab7ab92e2f71d4423ce05c2c39ef7ba3c71
duong91002 <nguyenhaiduong9102@gmail.com>
2024-02-28 21:01:16 +0700

    feat create favorite

--- Android/MovieApp/app/src/main/java/com/example/movieapp/FavoriteFragment.kt
+++ Android/MovieApp/app/src/main/java/com/example/movieapp/FavoriteFragment.kt
@@ -1,14 +1,10 @@
 package com.example.movieapp
 
-import MyAdapter
 import android.os.Bundle
+import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import androidx.fragment.app.Fragment
-import androidx.recyclerview.widget.GridLayoutManager
-import androidx.recyclerview.widget.RecyclerView
-
 
 // TODO: Rename parameter arguments, choose names that match
 // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
@@ -17,23 +13,13 @@ private const val ARG_PARAM2 = "param2"
 
 /**
  * A simple [Fragment] subclass.
- * Use the [HistoryFragment.newInstance] factory method to
+ * Use the [FavoriteFragment.newInstance] factory method to
  * create an instance of this fragment.
  */
-
-class HistoryFragment : Fragment() {
+class FavoriteFragment : Fragment() {
     // TODO: Rename and change types of parameters
     private var param1: String? = null
     private var param2: String? = null
-
-    private fun generateDataList(): List<DataItem> {
-        val dataList: MutableList<DataItem> = ArrayList()
-        dataList.add(DataItem(R.drawable.anime1, "Chú thuật hồi chiến", "2022"))
-        dataList.add(DataItem(R.drawable.anime2, "abc 2", "2023"))
-        dataList.add(DataItem(R.drawable.anime3, "abc 3", "2024"))
-        // Thêm các phần tử khác vào danh sách dữ liệu
-        return dataList
-    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -48,19 +34,7 @@ class HistoryFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        val view = inflater.inflate(R.layout.fragment_history, container, false)
-
-        val recyclerView = view.findViewById<RecyclerView>(R.id.recycler_view)
-//        recyclerView.layoutManager = LinearLayoutManager(requireContext())
-        recyclerView.layoutManager = GridLayoutManager(view.context, 2)
-
-        val dataList: List<DataItem>? = generateDataList() // Tạo danh sách dữ liệu
-
-        val adapter = dataList?.let { MyAdapter(it) } ?: MyAdapter(emptyList())
-        recyclerView.adapter = adapter
-
-        return view
-
+        return inflater.inflate(R.layout.fragment_favorite, container, false)
     }
 
     companion object {
@@ -70,12 +44,12 @@ class HistoryFragment : Fragment() {
          *
          * @param param1 Parameter 1.
          * @param param2 Parameter 2.
-         * @return A new instance of fragment HistoryFragment.
+         * @return A new instance of fragment FavoriteFragment.
          */
         // TODO: Rename and change types and number of parameters
         @JvmStatic
         fun newInstance(param1: String, param2: String) =
-            HistoryFragment().apply {
+            FavoriteFragment().apply {
                 arguments = Bundle().apply {
                     putString(ARG_PARAM1, param1)
                     putString(ARG_PARAM2, param2)
