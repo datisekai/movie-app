101bc99cfefc0930c3e348f4d58f205eaa526bae
duong91002 <nguyenhaiduong9102@gmail.com>
2024-03-21 18:33:02 +0700

    merge master3

--- Android/MovieApp/app/src/main/java/com/example/movieapp/ui/activity/DetailFilmActivity.kt
+++ Android/MovieApp/app/src/main/java/com/example/movieapp/ui/activity/DetailFilmActivity.kt
@@ -3,11 +3,9 @@ package com.example.movieapp.ui.activity
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-<<<<<<< HEAD:Android/MovieApp/app/src/main/java/com/example/movieapp/ui/activity/DetailFilmActivity.kt
-import com.example.movieapp.R
-=======
 import android.view.View
->>>>>>> master:Android/MovieApp/app/src/main/java/com/example/movieapp/DetailFilmActivity.kt
+import com.example.movieapp.PlayerActivity
+import com.example.movieapp.R
 
 class DetailFilmActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
