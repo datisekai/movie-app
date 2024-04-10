355293b5f71f7067588840525748c7c8a2a70cea
duong91002 <nguyenhaiduong9102@gmail.com>
2024-04-10 23:05:15 +0700

    feat call api login google

--- Android/MovieApp/app/src/main/java/com/example/movieapp/config.kt
+++ Android/MovieApp/app/src/main/java/com/example/movieapp/config.kt
@@ -1,5 +1,9 @@
 package com.example.movieapp
 
 public class config(){
-    val baseURL = "https://movie-backend.datisekai.id.vn"
+    companion object {
+        val BASE_URL = "https://movie-backend-ashy.vercel.app/"
+        val WEB_CLIENT_ID= "288864796717-ltmriich8rdbidlanccl4o2ti0estpid.apps.googleusercontent.com"
+    }
+
 }
