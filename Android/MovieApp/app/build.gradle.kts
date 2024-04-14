a4fc81b9d2fc6f682a9cecda1fec4678e0095223
duong91002 <nguyenhaiduong9102@gmail.com>
2024-04-14 14:52:40 +0700

    feat activity intro

--- Android/MovieApp/app/build.gradle.kts
+++ Android/MovieApp/app/build.gradle.kts
@@ -69,6 +69,7 @@ dependencies {
     implementation("androidx.compose.material3:material3")
 
     implementation("com.google.firebase:firebase-messaging:23.4.1")
+    implementation("androidx.activity:activity:1.8.0")
 
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
